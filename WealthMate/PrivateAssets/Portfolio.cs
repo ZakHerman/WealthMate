82e91c09c8370d7df86f1833762d309aba6217a2
Zakanator <zakanator@gmail.com>
2019-08-22 18:50:12 +1200

    Fix compile errors. Refactor portfolio and privatetermdeposit.

---
+++ WealthMate/PrivateAssets/Portfolio.cs
@@ -0,0 +1,33 @@
+﻿namespace WealthMate.PrivateAssets
+{
+    public class Portfolio
+    {
+        //Auto-implemented variables, from class diagram. "Owned Asset" is a separate class.
+        //private List<OwnedAsset> ownedAssets;
+        public float Total { get; set; }
+        public float DayReturn { get; set; }
+        public float TotalReturn { get; set; }
+        public float ReturnGoal { get; set; }
+
+        public Portfolio()
+        {
+            //ownedAssets = new List<OwnedAsset>();
+            Total = 0.00f;
+            DayReturn = 0.00f;
+            TotalReturn = 0.00f;
+            ReturnGoal = 0.00f;
+        }
+
+        /*public void AddAsset(OwnedAsset asset)
+    {
+        ownedAssets.Add(asset);
+        Console.WriteLine("Asset successfully added");
+    }
+
+    public void RemoveAsset(OwnedAsset asset)
+    {
+        ownedAssets.Remove(asset);
+        Console.WriteLine("Asset successfully removed");
+    }*/
+    }
+}
---
+++ WealthMate/PrivateAssets/PrivateTermDeposit.cs
@@ -0,0 +1,25 @@
+﻿namespace WealthMate.PrivateAssets
+{
+    public class PrivateTermDeposit
+    {
+        public float AmountInvested { get; set; }
+        public float InterestDecision { get; set; }
+        TermDeposit pTermDeposit;
+
+        public PrivateTermDeposit(float amountInvested, float interestDecision, TermDeposit pTermDeposit)
+        {
+            AmountInvested = amountInvested;
+
+        }
+
+        public void OwnedTermDeposit(TermDeposit pTermDeposit, float amountInvested, float interestDecision)
+        {
+            //pTermDeposit = new TermDeposit();
+        }
+
+        /*public override string ToString()
+        {
+            return base.ToString();
+        }*/
+    }
+}
