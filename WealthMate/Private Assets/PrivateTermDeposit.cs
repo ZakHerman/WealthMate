bdcf9a3c2f21d753fc18526961ee66d3198a8f7c
Gwenneth De Leon <gwenjdeleon@gmail.com>
2019-08-22 17:17:55 +1200

    Some work on the Term Deposit classes (public and private)

--- WealthMate/Private Assets/PrivateTermDeposit.cs
+++ WealthMate/Private Assets/PrivateTermDeposit.cs
@@ -2,8 +2,23 @@
 
 public class PrivateTermDeposit
 {
-	public PrivateTermDeposit()
+    private float amountInvested { get; set; }
+    private float interestDecision { get; set; }
+    TermDeposit pTermDeposit;
+
+    public PrivateTermDeposit(float amountInvested, float interestDecision, TermDeposit pTermDeposit)
 	{
+        this.amountInvested = amountInvested;
 
 	}
+
+    public void OwnedTermDeposit(TermDeposit pTermDeposit, float amountInvested, float interestDecision)
+    {
+        pTermDeposit = new TermDeposit();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 }
--- WealthMate/Public Assets/TermDeposit.cs
+++ WealthMate/Public Assets/TermDeposit.cs
@@ -2,11 +2,20 @@
 
 public class TermDeposit
 {
-    private float amount_invested;
-    private float interest_decision;
+    private string provider { get; set; }
+    private float minDeposit { get; set; }
+    private float maxDeposit { get; set; }
+    private float length { get; set; }
+    private float interestRate { get; set; }
 
-    public TermDeposit()
+    public TermDeposit(string provider, float minDeposit, float maxDeposit, float length, float interestRate)
 	{
-
+        this.provider = provider;
+        this.minDeposit = minDeposit;
+        this.maxDeposit = maxDeposit;
+        this.length = length;
+        this.interestRate = interestRate;
 	}
+
+    // TODO: hard code the term deposit info from each bank
 }
