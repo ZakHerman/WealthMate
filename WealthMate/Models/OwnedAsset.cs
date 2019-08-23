7bc685ed13585da2f2d4716c79235dded80d8b90
Jacob Garbarek <xnf3679@autuni.ac.nz>
2019-08-23 16:36:19 +1200

    Updated OwnedAsset and OwnedStock Classes

---
+++ WealthMate/Models/OwnedAsset.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class OwnedAsset
+{
+    private string AssetName { get; set; }
+    private string PurchaseDate { get; set; }
+    private int Length { get; set; }
+    private float InterestRate { get; set; }
+    private int CompoundRate { get; set; }
+    private float RegularPayment { get; set; }
+    protected virtual float PrincipalValue { get; set; }
+    private float _currentValue;
+    public virtual float CurrentValue
+    {
+        get
+        {
+            var NonPaymentValue = PrincipalValue * (float)Math.Pow((1 + (InterestRate / CompoundRate)), Length * CompoundRate);
+
+            if (RegularPayment > 0)
+                return (RegularPayment * (((float)Math.Pow((1 + (InterestRate / CompoundRate)), Length * CompoundRate) - 1 ) / (InterestRate / CompoundRate))) + NonPaymentValue;
+            else
+                return NonPaymentValue;
+        }
+    }
+    private float _totalReturn;
+    public float TotalReturn
+    {
+        get
+        {
+            _totalReturn = CurrentValue - PrincipalValue;
+            return _totalReturn;
+        }
+    }
+    private float _totalReturnRate;
+    public float TotalReturnRate
+    {
+        get
+        {
+            _totalReturnRate = (TotalReturn / PrincipalValue) * 100;
+            return _totalReturnRate;
+        }
+    }
+
+    public OwnedAsset(string assetName, string purchaseDate, float principalValue, float interestRate, int length, int compoundRate, float regularPayment)
+    {
+        AssetName = assetName;
+        PurchaseDate = purchaseDate;
+        RegularPayment = regularPayment;
+        PrincipalValue = principalValue;
+        InterestRate = interestRate;
+        Length = length;
+        CompoundRate = compoundRate;
+        _currentValue = CurrentValue;
+        _totalReturn = TotalReturn;
+        _totalReturnRate = TotalReturnRate;
+    }
+}
---
+++ WealthMate/Models/Public Assets/TermDeposit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class TermDeposit
+{
+    private float amount_invested;
+    private float interest_decision;
+
+    public TermDeposit()
+	{
+
+	}
+}
