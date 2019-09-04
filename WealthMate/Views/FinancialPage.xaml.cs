5378bc8f52fcf7dc285dcdfb0226c8bba7ea1a9a
Zakanator <zakanator@gmail.com>
2019-09-05 00:03:22 +1200

    Create tabbed page for financialpage.

---
+++ WealthMate/Views/FinancialPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WealthMate.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class FinancialPage : TabbedPage
+    {
+        public FinancialPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
