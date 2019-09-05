aadcc4fb6ea3353b50c7456b51553ea7e040ffde
Gwenneth De Leon <gms2879@autuni.ac.nz>
2019-09-05 17:12:29 +1200

    Logic for OwnedAssetTest

--- WealthMate.UnitTests/OwnedAssetTest.cs
+++ WealthMate.UnitTests/OwnedAssetTest.cs
@@ -4,14 +4,13 @@ using WealthMate.Models;
 namespace WealthMate.UnitTests
 {
     [TestClass]
-    public class OwnnedAssetTest
-    {
-
+    public class OwnedAssetTest
+    {   
         private OwnedAsset test1;
         private OwnedAsset test2;
         private OwnedAsset test3;
 
-        public OwnnedAssetTest()
+        public OwnedAssetTest()
         {
             test1 = new OwnedAsset("ANZ Bank", "18/03/18", "Term Deposit", 10000, 0.27f, 12, 4, 0);
             test2 = new OwnedAsset("ASB Bank", "08/01/19", "Term Deposit", 2000, 0.30f, 6, 4, 0);
