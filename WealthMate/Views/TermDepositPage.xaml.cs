1e0dedf8bfb98b4a3c1898a6f5e57fd5028cf3ea
Gwenneth De Leon <gms2879@autuni.ac.nz>
2019-09-07 20:36:29 +1200

    Term Deposit Page created

---
+++ WealthMate/Views/TermDepositPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WealthMate.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TermDepositPage : ContentPage
+    {
+        public TermDepositPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
