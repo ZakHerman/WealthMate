9a611a56ed5f17aa42f8900308209c882815f0c7
Zakanator <zakanator@gmail.com>
2019-09-08 18:28:35 +1200

    Add stockhistory class template. Add line graph of stockhistory.

---
+++ WealthMate/Models/StockHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WealthMate.Models
+{
+    public class StockHistory
+    {
+        public ObservableCollection<Stock> History{ get; } = new ObservableCollection<Stock>();
+
+        public StockHistory()
+        {
+            GenerateExample();
+        }
+
+        private void GenerateExample()
+        {
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,22), CurrentPrice = 1.23f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,23), CurrentPrice = 2.2f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,24), CurrentPrice = 4f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,25), CurrentPrice = 12.2f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,26), CurrentPrice = 5f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,27), CurrentPrice = 15f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,28), CurrentPrice = 3.25f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,29), CurrentPrice = 1.23f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,30), CurrentPrice = 1.23f});
+            History.Add(new Stock {PriceDate = new DateTime(2019,07,31), CurrentPrice = 2.52f});
+        }
+    }
+}
