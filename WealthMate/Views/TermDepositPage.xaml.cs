bab0cc0aa14981f09b32ceb6a4bf0049b4d8a59b
Gwenneth De Leon <gms2879@autuni.ac.nz>
2019-09-09 18:15:18 +1200

    Changed TD to tabbed, had to commit to pull.

--- WealthMate/Views/TermDepositPage.xaml.cs
+++ WealthMate/Views/TermDepositPage.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace WealthMate.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TermDepositPage : ContentPage
+    public partial class TermDepositPage : TabbedPage
     {
         public TermDepositPage()
         {
