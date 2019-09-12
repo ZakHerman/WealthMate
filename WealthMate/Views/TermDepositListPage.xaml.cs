ba682061635dd4963e5a65f1e9051e084afee8ef
Gwenneth De Leon <gms2879@autuni.ac.nz>
2019-09-12 17:29:04 +1200

    Term deposit list complete. Need to add swipe functionality.

--- WealthMate/Views/TermDepositListPage.xaml.cs
+++ WealthMate/Views/TermDepositListPage.xaml.cs
@@ -18,8 +18,16 @@ namespace WealthMate.Views
 
         public void GenerateExample()
         {
-            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 12.5f});
-            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 3});
+            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 12.5f, LengthInMonths = 6, MaxDeposit = 50000, MinDeposit = 10000 });
+            TermDeposits.Add(new TermDeposit {Logo = "ANZ", Provider = "ANZ", InterestRate = 4.5f, LengthInMonths = 12, MaxDeposit = 10000, MinDeposit = 1000});
+            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 3.6f, LengthInMonths = 9, MaxDeposit = 50000, MinDeposit = 10000 });
+            TermDeposits.Add(new TermDeposit {Logo = "ANZ", Provider = "ANZ", InterestRate = 4.5f, LengthInMonths = 18, MaxDeposit = 15000, MinDeposit = 1000 });
+            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 12.5f, LengthInMonths = 6, MaxDeposit = 8000, MinDeposit = 10000 });
+            TermDeposits.Add(new TermDeposit {Logo = "ANZ", Provider = "ANZ", InterestRate = 4.5f, LengthInMonths = 3, MaxDeposit = 0, MinDeposit = 1000 });
+            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 12.5f, LengthInMonths = 6, MaxDeposit = 1000000, MinDeposit = 1000 });
+            TermDeposits.Add(new TermDeposit {Logo = "ANZ", Provider = "ANZ", InterestRate = 4.5f, LengthInMonths = 24, MaxDeposit = 20000, MinDeposit = 1000 });
+            TermDeposits.Add(new TermDeposit {Logo = "WBC", Provider = "Westpac", InterestRate = 12.5f, LengthInMonths = 6, MaxDeposit = 15000, MinDeposit = 10000 });
+            TermDeposits.Add(new TermDeposit {Logo = "ANZ", Provider = "ANZ", InterestRate = 4.5f, LengthInMonths = 18, MaxDeposit = 1000, MinDeposit = 1000 });
         }
     }
 }
