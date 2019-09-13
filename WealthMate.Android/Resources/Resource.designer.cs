8de5293be86d2c508bd3ac3d6963302f2e48a9b3
Jacob Garbarek <xnf3679@autuni.ac.nz>
2019-09-13 13:48:54 +1200

    Added Watchlist Star

--- WealthMate.Android/Resources/Resource.designer.cs
+++ WealthMate.Android/Resources/Resource.designer.cs
@@ -5854,26 +5854,26 @@ namespace WealthMate.Droid
 			// aapt resource value: 0x7f020058
 			public const int avd_hide_password = 2130837592;
 			
-			// aapt resource value: 0x7f02014c
-			public const int avd_hide_password_1 = 2130837836;
-			
-			// aapt resource value: 0x7f02014d
-			public const int avd_hide_password_2 = 2130837837;
-			
 			// aapt resource value: 0x7f02014e
-			public const int avd_hide_password_3 = 2130837838;
+			public const int avd_hide_password_1 = 2130837838;
+			
+			// aapt resource value: 0x7f02014f
+			public const int avd_hide_password_2 = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int avd_hide_password_3 = 2130837840;
 			
 			// aapt resource value: 0x7f020059
 			public const int avd_show_password = 2130837593;
 			
-			// aapt resource value: 0x7f02014f
-			public const int avd_show_password_1 = 2130837839;
-			
-			// aapt resource value: 0x7f020150
-			public const int avd_show_password_2 = 2130837840;
-			
 			// aapt resource value: 0x7f020151
-			public const int avd_show_password_3 = 2130837841;
+			public const int avd_show_password_1 = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int avd_show_password_2 = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int avd_show_password_3 = 2130837843;
 			
 			// aapt resource value: 0x7f02005a
 			public const int BFG = 2130837594;
@@ -6556,11 +6556,11 @@ namespace WealthMate.Droid
 			// aapt resource value: 0x7f02013c
 			public const int notification_icon_background = 2130837820;
 			
-			// aapt resource value: 0x7f02014a
-			public const int notification_template_icon_bg = 2130837834;
+			// aapt resource value: 0x7f02014c
+			public const int notification_template_icon_bg = 2130837836;
 			
-			// aapt resource value: 0x7f02014b
-			public const int notification_template_icon_low_bg = 2130837835;
+			// aapt resource value: 0x7f02014d
+			public const int notification_template_icon_low_bg = 2130837837;
 			
 			// aapt resource value: 0x7f02013d
 			public const int notification_tile_bg = 2130837821;
@@ -6584,22 +6584,28 @@ namespace WealthMate.Droid
 			public const int SPK = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int TLS = 2130837828;
+			public const int starfilled = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int tooltip_frame_dark = 2130837829;
+			public const int starunfilled = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int tooltip_frame_light = 2130837830;
+			public const int TLS = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int uparrow = 2130837831;
+			public const int tooltip_frame_dark = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int WBC = 2130837832;
+			public const int tooltip_frame_light = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int xamarin_logo = 2130837833;
+			public const int uparrow = 2130837833;
+			
+			// aapt resource value: 0x7f02014a
+			public const int WBC = 2130837834;
+			
+			// aapt resource value: 0x7f02014b
+			public const int xamarin_logo = 2130837835;
 			
 			static Drawable()
 			{
