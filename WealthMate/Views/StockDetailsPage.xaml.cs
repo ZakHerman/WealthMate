c379a7d11bc1bdcfefc0e61c56569d782d1a7c9b
Jacob Garbarek <xnf3679@autuni.ac.nz>
2019-09-14 12:01:30 +1200

    WatchList Add/Remove (with star) Completed

--- WealthMate/Views/StockDetailsPage.xaml.cs
+++ WealthMate/Views/StockDetailsPage.xaml.cs
@@ -10,12 +10,29 @@ namespace WealthMate.Views
     {
         public Stock Stock { get; }
         public StockHistory StockHistory { get; }
+        public ObservableCollection<Stock> WatchListStocks { get; set; }
+        private bool _watched;
+        public bool Watched
+        {
+            get
+            {
+                return _watched;
+            }
+            set
+            {
+                _watched = WatchListStocks.Contains(Stock);
+            }
+        }
 
         public StockDetailsPage(Stock stock)            //Displays details of selected stock
         {
             Stock = stock;
             stock.UpdateStock();
             StockHistory = new StockHistory();
+
+            WatchListStocks = (Application.Current as App).User.WatchListStocks;
+            Watched = _watched;
+
             InitializeComponent();
 
             BindingContext = this;     
@@ -24,9 +41,16 @@ namespace WealthMate.Views
         private void WatchListStarClicked(object sender, System.EventArgs e)
         {
             if ((Application.Current as App).User.WatchListStocks.Contains(Stock))
+            {
                 (Application.Current as App).User.WatchListStocks.Remove(Stock);
+                (sender as ImageButton).Source = "starunfilled.png";
+            }
             else
+            {
                 (Application.Current as App).User.WatchListStocks.Add(Stock);
+                (sender as ImageButton).Source = "starfilled.png";
+            }
+               
         }
     }
 }
