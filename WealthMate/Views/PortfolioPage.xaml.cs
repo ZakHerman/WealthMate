d9006bd30f768d7ff1a45c5c568a2924ad838845
Devin Grant-Miles <yfk3228@autuni.ac.nz>
2019-09-17 10:09:03 +1200

    Search functionality for portfolio list

--- WealthMate/Views/PortfolioPage.xaml.cs
+++ WealthMate/Views/PortfolioPage.xaml.cs
@@ -17,6 +17,7 @@ namespace WealthMate.Views
     [DesignTimeVisible(false)]
     public partial class PortfolioPage
     {
+        private SearchBar _searchBar;
         public float TotalValue { get; set; }
         public Portfolio CurrentPortfolio { get; set; }
         public List<OwnedAsset> OwnedAssets { get; set; }
@@ -74,6 +75,39 @@ namespace WealthMate.Views
             pieChart.Add(_bond);
             pieChart.Add(_termD);
             pieChart.Add(_stock);
+
+
+        }
+
+        /// <summary>
+        /// Search bar functionality
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            _searchBar = (sender as SearchBar);
+
+            if (List.DataSource != null)
+            {
+                List.DataSource.Filter = FilterAssets;
+                List.DataSource.RefreshFilter();
+            }
+        }
+
+        /// <summary>
+        /// method for filtering the list view as text changes within the search bar
+        /// May need to be updated as the lists display is refined i.e. includes stocks and stock names
+        /// </summary>
+        /// <param name="obj"></param> 
+        /// <returns></returns>
+        private bool FilterAssets(object obj)
+        {
+            if (_searchBar?.Text == null)
+                return true;
+
+            return obj is OwnedAsset asset && (asset.AssetName.ToLower().Contains(_searchBar.Text.ToLower())
+                                          || asset.Type.ToLower().Contains(_searchBar.Text.ToLower()));
         }
     }
 }
