9c4cc38265c560a04f543aa446f293364c47b015
Jacob Garbarek <xnf3679@autuni.ac.nz>
2019-10-03 21:31:13 +1300

    Fixed Colour Change of Total Return in OwnedStock Page

--- WealthMate/App.xaml.cs
+++ WealthMate/App.xaml.cs
@@ -26,8 +26,8 @@ namespace WealthMate
             User.Portfolio.OwnedAssets.Add(new OwnedAsset("Westpac", new System.DateTime(2019, 09, 09, 0, 0, 0), "Term Deposit", 12f, 1.2f, 12, 1, 3, 2f));
             User.Portfolio.OwnedAssets.Add(new OwnedAsset("Air New Zealand", new System.DateTime(2019, 09, 06, 0, 0, 0), "Term Deposit", 12f, 2.4f, 9, 1, 3, 2f));
             User.Portfolio.OwnedAssets.Add(new OwnedAsset("Test2", new DateTime(2016, 1, 14), "Bond", 1000, 0.04f, 3, 1, 40, 200));
-            User.Portfolio.OwnedAssets.Add(new OwnedStock(new Stock{CompanyName = "Burger Fuel", CurrentPrice = 42.2f, LastTrade = new DateTime(2019, 09, 09, 0, 0, 0), Shares = 4, Volume = 4}, new System.DateTime(2019, 09, 09, 0, 0, 0), 50.0f, 100, 1000f));
-            User.Portfolio.OwnedAssets.Add(new OwnedStock(new Stock { CompanyName = "Spark", CurrentPrice = 3.24f, LastTrade = new DateTime(2019, 09, 09, 0, 0, 0), Shares = 4, Volume = 4 }, new System.DateTime(2019, 09, 09, 0, 0, 0), 3.00f, 150, 500f));
+            User.Portfolio.OwnedAssets.Add(new OwnedStock(new Stock{CompanyName = "Burger Fuel", CurrentPrice = 42.2f, LastTrade = new DateTime(2019, 09, 09, 0, 0, 0), Shares = 4, Volume = 4, PriceClose = 42.4f}, new System.DateTime(2019, 09, 09, 0, 0, 0), 50.0f, 100, 1000f));
+            User.Portfolio.OwnedAssets.Add(new OwnedStock(new Stock { CompanyName = "Spark", CurrentPrice = 3.24f, LastTrade = new DateTime(2019, 09, 09, 0, 0, 0), Shares = 4, Volume = 4, PriceClose = 3.20f}, new System.DateTime(2019, 09, 09, 0, 0, 0), 3.00f, 150, 500f));
             User.Portfolio.UpdatePortfolio();
         }
 
