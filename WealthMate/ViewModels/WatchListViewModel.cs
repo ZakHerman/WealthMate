4238cd84a11f9e236c481a31bac3b4e648817745
Zakanator <zakanator@gmail.com>
2019-10-10 12:38:05 +1300

    Refactor watchlist viewmodel.

---
+++ WealthMate/ViewModels/WatchListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using WealthMate.Models;
+
+namespace WealthMate.ViewModels
+{
+    public class WatchListViewModel
+    {
+        public ObservableCollection<Stock> WatchListStocks { get; set; }
+
+        public WatchListViewModel()
+        {
+            WatchListStocks = App.WatchList;
+
+            foreach (var stock in WatchListStocks)
+                stock.UpdateStock();
+        }
+    }
+}
