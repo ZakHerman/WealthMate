aaf2dd916275464a3052b44e825343738b7fc232
Zakanator <zakanator@gmail.com>
2019-10-22 20:43:19 +1300

    Add persistence to portfolio goal setting.

--- WealthMate/Helpers/Settings.cs
+++ WealthMate/Helpers/Settings.cs
@@ -1,12 +1,14 @@
 ﻿using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace WealthMate.Helpers
 {
-    class Settings
+    public class Settings
     {
         public static void Load()
         {
             Helper.SetTheme(Preferences.Get("Theme", "Light"));
+            ((App)Application.Current).User.Portfolio.EditPortfolioGoal(Preferences.Get("PortfolioGoal", 0f));
         }
     }
 }
--- WealthMate/Views/Settings/SettingsPage.xaml.cs
+++ WealthMate/Views/Settings/SettingsPage.xaml.cs
@@ -4,6 +4,7 @@ using Syncfusion.SfNumericTextBox.XForms;
 using Syncfusion.XForms.ComboBox;
 using WealthMate.Helpers;
 using WealthMate.Themes;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using SelectionChangedEventArgs = Syncfusion.XForms.ComboBox.SelectionChangedEventArgs;
@@ -49,6 +50,7 @@ namespace WealthMate.Views.Settings
             var newPortfolioGoal = float.Parse(editPortfolioReturnGoal.Value.ToString());
 
             ((App)Application.Current).User.Portfolio.EditPortfolioGoal(newPortfolioGoal);
+            Preferences.Set("PortfolioGoal", newPortfolioGoal);
         }
     }
 }
