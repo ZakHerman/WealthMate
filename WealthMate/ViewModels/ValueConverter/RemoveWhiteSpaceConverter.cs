74a8f0fb54d4e831b17f354da4349b69cdf79abd
Zakanator <zakanator@gmail.com>
2019-10-24 19:57:37 +1300

    Add company logos.

---
+++ WealthMate/ViewModels/ValueConverter/RemoveWhiteSpaceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace WealthMate.ViewModels.ValueConverter
+{
+    public class RemoveWhiteSpaceConverter : IValueConverter
+    {
+        // Remove all white spaces from string
+        // Used for getting company names for displaying logo
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Concat(value.ToString().ToCharArray().Where(c => !char.IsWhiteSpace(c)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
